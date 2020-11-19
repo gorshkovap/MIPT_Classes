d211249d42b91b9283ed645797b980116a6cd840
gorshkovap <73563584+gorshkovap@users.noreply.github.com>
2020-11-19 17:19:50 +0300

    Add files via upload

---
+++ HW 11/ex2.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <type_traits>
+
+template <typename T>
+struct remove_const
+{
+	using type = T;
+};
+
+template <typename T>
+struct remove_const<const T> 
+{
+	using type = T;
+};
+
+template <typename T>
+using remove_const_t = typename remove_const<T>::type;
+
+template < typename T >
+struct add_const
+{
+	using type = const T;
+};
+
+template <typename T>
+using add_const_t = typename add_const<T>::type; 
+
+template <typename T1, typename T2>
+bool is_same_v = std::is_same<T1, T2>::value; 
+
+int main()
+{
+	const int x = 0;
+	int y = 0;
+
+	remove_const_t<const int> a = y;
+	add_const_t<int> b = x;
+
+	std::cout << std::boolalpha;
+
+	std::cout << is_same_v<decltype(a), int> << '\n';
+	std::cout << is_same_v<decltype(b), const int> << '\n';
+
+	return 0;
+}
---
+++ HW 11/ex4.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <type_traits>
+
+template <bool B, typename T = void> 
+struct enable_if
+{
+	using type = T;
+};
+
+template <typename T>
+struct enable_if<false, T> {}; 
+
+template <bool B, typename T = void>
+using enable_if_t = typename enable_if<B, T>::type; 
+
+template <typename T1, typename T2>
+inline const bool is_same_v = std::is_same<T1, T2>::value; 
+
+template <typename T>
+enable_if_t<is_same_v<T, int>, T> f(T t)
+{
+	return t;
+}
+
+int f(double d)
+{
+	return 2;
+}
+
+int main()
+{
+	std::cout << std::boolalpha;
+
+	std::cout << is_same_v <enable_if_t<(1 > 0), int>, int> << '\n';
+
+	if (f('c') == 2)
+	{
+		std::cout << "All right";
+	}
+
+	return 0;
+}
