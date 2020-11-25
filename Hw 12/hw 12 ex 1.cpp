586a2e2430f9213852bcda74b9735a0cda80e8cd
gorshkovap <73563584+gorshkovap@users.noreply.github.com>
2020-11-26 02:37:38 +0300

    Update hw 12 ex 1.cpp

--- Hw 12/hw 12 ex 1.cpp
+++ Hw 12/hw 12 ex 1.cpp
@@ -1,28 +1,27 @@
-﻿// hw 12 ex 1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+// hw 12 ex 1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
 
 #include <iostream>
 
 template <int n, int k>  //c
 
-	static const int comb = Combinations<k - 1, n - 1> + Combinations<k, n - 1>;
+constexpr int Combinations = Combinations<n-1, k> +Combinations<n-1, k - 1>;
 
 
 
 template<int n>
-
-	static const int comb<0,n> = 1;
+constexpr int Combinations<n, 0> = 1;
 
 
 template <int n>
 
 
-	static const int comb<n,n> = 1;
+constexpr int Combinations<n, n> = 1;
 
 
 int main(int argc, char** argv)
 {
-	constexpr int result = Combinations<4, 1>::value;
+	constexpr int result = Combinations<10, 2>;
 	std::cout << result << std::endl;
 
 	system("pause");
